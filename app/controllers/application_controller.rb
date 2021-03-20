47ea02487c702bcfac42201acbd983ad6c5bc0ca
Gruinch <sel.chemi86@gmail.com>
2021-03-20 23:20:52 +0000

    [Edit]viewファイル

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
   end
-
-  def after_sign_up_path_for(resource)
-    users_path
-  end
+  
+  
 end
