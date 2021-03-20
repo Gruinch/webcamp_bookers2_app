52462e10c221d5f4702052a1ca1929d730c77cc5
Gruinch <sel.chemi86@gmail.com>
2021-03-20 11:32:11 +0000

    [add] top page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,20 @@
 class BooksController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def edit
+  
+  def destroy
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:'homes#top'
+  get "about"=>"homes#about",as:"about"
+  resources :books,only:[:new, :create, :show, :index, :edit, :destroy]
+  resources :users,only:[:show, :edit, :update, :inde]
+  
 end
